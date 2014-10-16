e87faa6a469fa01e0ae42d65f9ba0c793ad83010
Alice Rhee <alice.rhee12@gmail.com>
2014-10-16 11:01:24 -0400

    More onClick stuff
    onClick stuff - Rhee

--- SI543-week6-WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/MyActivity.java
+++ SI543-week6-WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/MyActivity.java
@@ -1,9 +1,11 @@
 package com.example.si543.whatsyourstory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MyActivity extends Activity {
@@ -33,4 +35,14 @@ public class MyActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-}
+    public void LogIn (View view) {
+        Intent LogInActivity = new Intent(this, LogInActivity.class);
+        startActivity(LogInActivity);
+    }
+
+    public void SignUp (View view){
+        Intent SignUpActivity = new Intent(this, SignUpActivity.class);
+        startActivity(SignUpActivity);
+    }
+    }
+
