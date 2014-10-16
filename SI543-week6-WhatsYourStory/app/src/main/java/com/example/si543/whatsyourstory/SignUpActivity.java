a4bf4e8b66fa1016b0bb4a6b2b2f71a2c7eac40b
Alice Rhee <alice.rhee12@gmail.com>
2014-10-16 11:01:00 -0400

    Rhee - adding onCreate and onClick
    Added those methods/bundles to my files

--- SI543-week6-WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/SignUpActivity.java
+++ SI543-week6-WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/SignUpActivity.java
@@ -4,14 +4,21 @@ package com.example.si543.whatsyourstory;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 
 public class SignUpActivity extends Activity {
-    public void signupNext (View view) {
-        //onClick Next button
-        //verify un, password, confirm password, email info
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+
+    public void SignUpNext (View view) {
         Intent SetProfileActivity = new Intent(this, SetProfileActivity.class);
         startActivity(SetProfileActivity);
+        finish(); //finishes the activity/destroys it?
     }
 }
 
+//onClick Next button
+//verify un, password, confirm password, email info
--- SI543-week6-WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/UserProfileActivity.java
+++ SI543-week6-WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/UserProfileActivity.java
@@ -4,9 +4,17 @@ package com.example.si543.whatsyourstory;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 
 public class UserProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profileview);
+    }
+
     public void Message (View view){
         Intent messageIntent = new Intent (this, MessageActivity.class);
         startActivity(messageIntent);
