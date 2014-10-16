695bfdbf996bcb705f16310ebd9966365d897e34
Chia Hui Hsieh <chiahuih@umich.edu>
2014-10-16 10:47:16 -0400

    add onCreation on Feed_View class

--- SI543-week6-WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/SignUpActivity.java
+++ SI543-week6-WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/SignUpActivity.java
@@ -4,9 +4,17 @@ package com.example.si543.whatsyourstory;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 
 public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+
     public void signupNext (View view) {
         //onClick Next button
         //verify un, password, confirm password, email info
