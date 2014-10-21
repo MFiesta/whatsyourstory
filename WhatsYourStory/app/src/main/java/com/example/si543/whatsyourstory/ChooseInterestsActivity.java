dd792a6b0b4ca66fbc464dac25ceddc0a7aff43c
mfiesta <mfiesta@umich.edu>
2014-10-21 07:40:01 -0400

    Rename "ChooseInterests" to "ChooseInterestsActivity"

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/ChooseInterestsActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/ChooseInterestsActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ChooseInterests extends Activity {
+public class ChooseInterestsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
