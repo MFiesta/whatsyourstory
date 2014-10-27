0c60b25e6c9e3687d3b9c55ed4c18f0f49823345
Chia Hui Hsieh <chiahuih@umich.edu>
2014-10-27 12:14:16 -0400

    Create an action bar icon on OtherUserProfile

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
@@ -5,6 +5,8 @@ package com.example.si543.whatsyourstory;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class OtherUserProfileActivity extends Activity {
@@ -26,6 +28,25 @@ public class OtherUserProfileActivity extends Activity {
         startActivity(favoriteIntent);
     }
 
+//Action Bar - star
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.other_user_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 //visibility contingent on user's approval to see their calendar
 
 //    public void ViewCalendar (View view){
