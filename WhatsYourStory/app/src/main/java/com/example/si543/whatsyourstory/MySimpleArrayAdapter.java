2b19d8492803555e0d83e7e5472a092e84f0462d
Chia Hui Hsieh <chiahuih@umich.edu>
2014-10-29 03:22:15 -0400

    set rowView in MySimpleArrayAdapter

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/MySimpleArrayAdapter.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/MySimpleArrayAdapter.java
@@ -1,12 +1,15 @@
 package com.example.si543.whatsyourstory;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import java.io.File;
 import java.util.ArrayList;
 
@@ -14,9 +17,12 @@ import java.util.ArrayList;
  * Created by chiahuihsieh on 10/29/14.
  */
 public class MySimpleArrayAdapter extends ArrayAdapter<FeedUserData> {
+    private int mResource = 0;
+
     public MySimpleArrayAdapter(Context context, int layoutResourceId, ArrayList<FeedUserData> values) {
 //http://developer.android.com/reference/android/widget/ArrayAdapter.html#ArrayAdapter(android.content.Context, int, java.util.List<T>)
         super(context, layoutResourceId, values);
+        mResource = layoutResourceId;
 
     }
 
@@ -24,7 +30,11 @@ public class MySimpleArrayAdapter extends ArrayAdapter<FeedUserData> {
     public View getView(int position, View convertView, ViewGroup parent) {
 //modify the id to match what you have in your xml file (passed when you use “new MySimpleArrayAdapter(context, layoutResourceId, values)”
 //fetch corresponding view
-        View rowView = super.getView(position, convertView, parent);
+        View rowView = convertView;
+        if (rowView == null) {
+            LayoutInflater inflater = ((Activity)getContext()).getLayoutInflater();
+            rowView = inflater.inflate(mResource, parent, false);
+        }
         TextView nameTextView = (TextView) rowView.findViewById(R.id.feedName);
         TextView careerTextView = (TextView) rowView.findViewById(R.id.feedCareer);
         TextView locationTextView = (TextView) rowView.findViewById(R.id.feedLocation);
