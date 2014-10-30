f8b70c9aabd43294922e370cb44698b0d744b224
Chia Hui Hsieh <chiahuih@umich.edu>
2014-10-30 02:40:47 -0400

    Modify adapter file and add pictures to the List

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/MySimpleArrayAdapter.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/MySimpleArrayAdapter.java
@@ -2,6 +2,7 @@ package com.example.si543.whatsyourstory;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -30,25 +32,39 @@ public class MySimpleArrayAdapter extends ArrayAdapter<FeedUserData> {
     public View getView(int position, View convertView, ViewGroup parent) {
 //modify the id to match what you have in your xml file (passed when you use “new MySimpleArrayAdapter(context, layoutResourceId, values)”
 //fetch corresponding view
+
+
+
         View rowView = convertView;
         if (rowView == null) {
+            //Row inflation
             LayoutInflater inflater = ((Activity)getContext()).getLayoutInflater();
             rowView = inflater.inflate(mResource, parent, false);
         }
+        //Get reference to TextView - feedName
         TextView nameTextView = (TextView) rowView.findViewById(R.id.feedName);
+        //Get reference to TextView - feedCareer
         TextView careerTextView = (TextView) rowView.findViewById(R.id.feedCareer);
+        // Get reference to TextView - feedLocation
         TextView locationTextView = (TextView) rowView.findViewById(R.id.feedLocation);
+
+        //Get reference to image view
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
         //set data for the view
         FeedUserData user = getItem(position);
         nameTextView.setText(user.getName());
         careerTextView.setText(user.getCareer());
         locationTextView.setText(user.getLocation());
-        String imagePath = user.getImagePath();
+        String imagePath = user.getImagePath(); //assume image is under assets folder
         if (imagePath != null) {
-            File file = new File(imagePath);
-            Uri uri = Uri.fromFile(file);
-            imageView.setImageURI(uri);
+
+            try {
+                imageView.setImageBitmap(BitmapFactory.decodeStream(((Activity) getContext()).getAssets().open(imagePath)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
 
         return rowView;
