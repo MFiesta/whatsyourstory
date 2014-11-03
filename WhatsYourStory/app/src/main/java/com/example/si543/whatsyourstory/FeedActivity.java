42e1ba99e924534b76cd5492a49bb42993c669d9
Stephanie Wooten <srwooten@gmail.com>
2014-11-03 15:45:45 -0500

    Removed previous experimentation
    Attempts at making navigation drawer didn’t work - starting over from
    scratch again

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/FeedActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/FeedActivity.java
@@ -1,43 +1,33 @@
 package com.example.si543.whatsyourstory;
 
-
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.SearchManager;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 
-public class FeedActivity extends Activity {
-    implements FragmentNavigationDrawer.NavigationDrawer
+public class FeedActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
+
         ListView feedListView = (ListView) findViewById(R.id.feedListView);
 
-        // set adapter for feed listview
+        // set adatper for this listview
         ArrayList<FeedUserData> values = new ArrayList<FeedUserData>();
 
         FeedUserData user = new FeedUserData("Eytan Adar", "Professor at UM", "Ann Arbor", "adar_eytan.png");
@@ -48,24 +38,17 @@ public class FeedActivity extends Activity {
         values.add(user);
 
 
-        MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, R.layout.listitem_in_activity_feed, values);
+        MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this,R.layout.listitem_in_activity_feed, values);
         feedListView.setAdapter(adapter);
 
-        //set listview onitemclicklistener for feedList
+        //set listview onitemclicklistener
         feedListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 startActivity(new Intent(FeedActivity.this, OtherUserProfileActivity.class));
-
-
             }
         });
-
 
     }
 
 }
-
-
-
-
