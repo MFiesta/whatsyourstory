1fbb542460086191218bcfb0cab6fc13a1b5014a
mfiesta <mfiesta@umich.edu>
2014-11-04 22:54:46 -0500

    Attempt to save ToggleButton data in Choose Skills page with SharedPreferences

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/ChooseSkillsActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/ChooseSkillsActivity.java
@@ -6,6 +6,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.content.SharedPreferences;
+import android.widget.ToggleButton;
+
 
 public class ChooseSkillsActivity extends Activity {
 
@@ -18,6 +21,32 @@ public class ChooseSkillsActivity extends Activity {
     public void skillsNext(View view) {
         Intent ChooseSkillsActivity = new Intent(this, ChooseInterestsActivity.class);
         startActivity(ChooseSkillsActivity);
+    }
+
+    private ToggleButton toggle;
+
+    @Override
+    public void onClick(View v)
+    {
+        if (toggle.isChecked())
+        {
+            SharedPreferences.Editor editor = getSharedPreferences("com.example.si543.whatsyourstory", MODE_PRIVATE).edit();
+            editor.putBoolean("NameOfThingToSave", true);
+            editor.commit();
+        }
+        else
+        {
+            SharedPreferences.Editor editor = getSharedPreferences("com.example.si543.whatsyourstory", MODE_PRIVATE).edit();
+            editor.putBoolean("NameOfThingToSave", false);
+            editor.commit();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        SharedPreferences sharedPrefs = getSharedPreferences("com.example.si543.whatsyourstory", MODE_PRIVATE);
+        toggle.setChecked(sharedPrefs.getBoolean("NameOfThingToSave", true));
     }
 
     //Takes User to ChooseInterestsActivity after they select which skills they have
