6ea03fdacf9185c1239563baf33f7556e767b170
Stephanie Wooten <srwooten@gmail.com>
2014-11-10 16:20:20 -0500

    Removed extraneous import for msging

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.net.Uri;
 
 public class OtherUserProfileActivity extends Activity {
 
