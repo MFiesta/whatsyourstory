4b2f571e2d7df9acb3770a8bd97f04fd95edb265
Stephanie Wooten <srwooten@gmail.com>
2014-11-20 10:27:21 -0500

    Removed LogOut from NavDrawer

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/FeedActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/FeedActivity.java
@@ -81,8 +81,6 @@ public class FeedActivity extends Activity {
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], true, "3"));
         //Favorites - Do we need a counter for this?
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[3]));
-        //Log Out
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4]));
 
         //set the nav drawer list adapter
         adapter = new NavDrawerListAdapter(getApplicationContext(), navDrawerItems);
@@ -249,11 +247,6 @@ public class FeedActivity extends Activity {
             case 3:
                 Intent intent = new Intent(FeedActivity.this, FavoritesActivity.class);
                 startActivity(intent);
-                break;
-            //If user selects "Log Out" - Takes them to homepage
-            case 4:
-                Intent logOut = new Intent(FeedActivity.this, LogInActivity.class);
-                startActivity(logOut);
                 break;
             default:
                 break;
