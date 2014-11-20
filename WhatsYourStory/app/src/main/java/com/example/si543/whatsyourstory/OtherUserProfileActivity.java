dc2d41e9d73b48896338a60012f1775c1b72b883
Stephanie Wooten <srwooten@gmail.com>
2014-11-20 12:29:35 -0500

    Merge branch 'master' into messages_sharedpref
    Conflicts:
    
    WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/Messag
    esActivity.java
    
    WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/Shared
    PreferencesUtility.java

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
@@ -99,16 +99,16 @@ public class OtherUserProfileActivity extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    //@Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        //int id = item.getItemId();
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+        //return super.onOptionsItemSelected(item);
     }
 //visibility contingent on user's approval to see their calendar
 
@@ -124,4 +124,3 @@ public class OtherUserProfileActivity extends Activity {
     // private void initlist(){
     //   List.add(createskill("skiing")
     // }
-}
--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/SetMyInitProfileActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/SetMyInitProfileActivity.java
@@ -4,16 +4,32 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 //Stephanie Wooten
-
+//Michelle Fiesta added SharedPreferences stuff
 
 public class SetMyInitProfileActivity extends Activity {
+
+    //Full name, title, company, location edittext
+    EditText edit_full_name, edit_current_title, edit_current_company, edit_current_location;
+
+    //Session Manager class
+    SessionManagement session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_init_profile_settings);
+
+        //Session Manager
+        session = new SessionManagement(getApplicationContext());
+
+        //Full name, title, company, location input text
+        edit_full_name = (EditText) findViewById(R.id.edit_full_name);
+        edit_current_title = (EditText) findViewById(R.id.edit_current_title);
+        edit_current_company = (EditText) findViewById(R.id.edit_current_company);
+        edit_current_location = (EditText) findViewById(R.id.edit_current_location);
     }
 
     public void MyInitProfileNext(View view) {
