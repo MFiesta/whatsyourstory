b6b174798ec405f1e5fd0a1284b150051ceae861
Chia Hui Hsieh <chiahuih@umich.edu>
2014-11-24 08:36:53 -0500

    delete log out button on feed page

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/FeedActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/FeedActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.app.NavUtils;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
