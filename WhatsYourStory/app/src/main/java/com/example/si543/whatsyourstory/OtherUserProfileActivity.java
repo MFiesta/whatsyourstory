000b9c1fd4e9cc15482e30d68279756ecb51a2f4
Chia Hui Hsieh <chiahuih@umich.edu>
2014-11-25 09:24:33 -0500

    modify other user profile design

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
