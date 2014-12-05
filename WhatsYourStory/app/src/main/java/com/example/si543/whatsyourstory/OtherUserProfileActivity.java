83c4b331250035b9c799ca4b2e918b419a6af9fd
Stephanie Wooten <srwooten@gmail.com>
2014-12-04 19:39:35 -0500

    Removed calendar

--- WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
+++ WhatsYourStory/app/src/main/java/com/example/si543/whatsyourstory/OtherUserProfileActivity.java
@@ -127,17 +127,4 @@ public class OtherUserProfileActivity extends Activity {
         //}
         //return super.onOptionsItemSelected(item);
     }
-//visibility contingent on user's approval to see their calendar
 
-//    public void ViewCalendar (View view){
-//        Intent calendarIntent = new Intent (this, CalendarActivity.class);
-//        startActivity(calendarIntent);
-//    }
-    //fill in list items from the other user's Choose interests Activity screen
-    //List-set up 2 lists here for the skills and experience of the user
-    //skills list = new ArrayList<~>();
-    //  initList();
-    //   ListView skillsListView = (Listview)
-    // private void initlist(){
-    //   List.add(createskill("skiing")
-    // }
